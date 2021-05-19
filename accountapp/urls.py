ac4305adcca14ee23487192d248a674b2ac6d1c9
soyolim-txter <healingdaybut@gmail.com>
2021-05-19 20:52:55 +0900

    Delete redundant files

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -4,6 +4,6 @@ from accountapp.views import hello_world, start_world
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name = 'hello_world'),
+    path('hello_world/',hello_world, name = 'hello_world'),
     path('start_world/',start_world, name = 'start_world')
 ]
