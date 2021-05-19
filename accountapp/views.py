28324cbbe7268dd3e21731cbf42bec9f7f621763
soyolim-txter <healingdaybut@gmail.com>
2021-05-19 22:35:37 +0900

    django course 8

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return HttpResponse("HI SOYOLIM")
+    return render(request,"base.html")
 
 
 def start_world(request):
     return HttpResponse("SUCESS YOURSELF")
 # 문자열을 반환 해주는 중
+
