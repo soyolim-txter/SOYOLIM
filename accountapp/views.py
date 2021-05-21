f9cff99338a55bc2497e757633d3c1c283b5f983
soyolim-txter <healingdaybut@gmail.com>
2021-05-21 20:40:55 +0900

    django course 18th adding httpResponseRedirect

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 # Create your views here.
 from accountapp.models import HelloWorld
 
@@ -13,9 +14,11 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
         # DB 관련
-        return render(request,"accountapp/hello_world.html",context={'hello_world_output': new_hello_world })
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request,"accountapp/hello_world.html",context={'text':'GET METHOD!!!"'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request,"accountapp/hello_world.html",context={'hello_world_list': hello_world_list})
 
 def start_world(request):
     return HttpResponse("SUCESS YOURSELF")
