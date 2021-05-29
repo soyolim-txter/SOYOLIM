d7459286cca46dfde0601bb7ab1ec86c47766f12
soyolim-txter <healingdaybut@gmail.com>
2021-05-29 21:52:24 +0900

    django CRUD alpha version fin

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 # Create your views here.
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
@@ -40,3 +40,8 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url =  reverse_lazy("accountapp:hello_world")
     template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
