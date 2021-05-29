1b8bde7c03deff214cfff3b08a888d4f4a89707d
soyolim-txter <healingdaybut@gmail.com>
2021-05-29 16:43:50 +0900

    django createview and login, logout

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 # Create your views here.
-from accountapp.models import HelloWorld
+from django.views.generic import CreateView
 
+from accountapp.models import HelloWorld
 
 def hello_world(request):
 
@@ -20,7 +23,9 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request,"accountapp/hello_world.html",context={'hello_world_list': hello_world_list})
 
-def start_world(request):
-    return HttpResponse("SUCESS YOURSELF")
-# 문자열을 반환 해주는 중
+class AccountCreateView(CreateView):
+    model =  User
+    form_class = UserCreationForm
+    success_url =  reverse_lazy("accountapp:hello_world")
+    template_name = 'accountapp/create.html'
 
