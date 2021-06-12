8f44199a5d31e3cc38f84f035be4e7eb61925fc7
soyolim-txter <healingdaybut@gmail.com>
2021-06-12 20:23:53 +0900

    django projectapp initial version fin.

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView
+from projectapp.views import ProjectCreateView, ProjectListView, ProjectDetailView
 
 app_name = 'projectapp'
 
 urlpatterns = [
-    # path('',ProjectListView.as_view(),name='list'),
-    path('craate/',ProjectCreateView.as_view(),name='create'),
-    # path('detail/<int:pk>',ProjectDetailView.as_view(),name='detail')
+    path('',ProjectListView.as_view(),name='list'),
+    path('create/',ProjectCreateView.as_view(),name='create'),
+    path('detail/<int:pk>',ProjectDetailView.as_view(),name='detail'),
 ]
--- projectapp/views.py
+++ projectapp/views.py
@@ -1,21 +1,32 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, ListView, DetailView
 
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
 
-
+@method_decorator(login_required,'get')
+@method_decorator(login_required,'post')
 class ProjectCreateView(CreateView):
     model = Project
     form_class = ProjectCreationForm
     template_name = 'projectapp/create.html'
 
-    def form_valid(self, form):
-
-        return super().form_valid(form)
-
     def get_success_url(self):
         return reverse('projectapp:detail',kwargs={'pk':self.object.pk})
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'projectapp/list.html'
+    context_object_name = 'project_list'
+    paginate_by = 25
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'projectapp/detail.html'
+    context_object_name = 'target_project'
+
