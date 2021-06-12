a37774e5578e35d0664492425e6ae63bce8507d2
soyolim-txter <healingdaybut@gmail.com>
2021-06-12 17:24:17 +0900

    django commentapp fin course 38th

--- commentapp/models.py
+++ commentapp/models.py
@@ -9,4 +9,4 @@ class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.SET_NULL, null=True, related_name='comment')
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='comment')
     content = models.TextField(null=False)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
