730a526e656e6d19c6c3a15c56225f46bc56d797
soyolim-txter <healingdaybut@gmail.com>
2021-06-13 23:50:04 +0900

    django WYSIWYG fin

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,17 @@
 from django.forms import ModelForm
+from django import forms
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    # https: // wayhome25.github.io / django / 2017 / 05 / 06 / django - form /
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'editable text-left',
+                                                           'style':'height:auto; text-align:left'}))
+
+    project = forms.ModelChoiceField(queryset=Project.objects.all(),required=False)
+
     class Meta:
         model = Article
         fields = ['title', 'image', 'project', 'content']
