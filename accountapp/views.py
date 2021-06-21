a28e3137450d2478cd28c0b70f2ef5fd22edd2fb
soyolim-txter <healingdaybut@gmail.com>
2021-06-21 17:24:00 +0900

    django fin

--- accountapp/views.py
+++ accountapp/views.py
@@ -18,7 +18,7 @@ has_ownership = [login_required,account_ownership_requred]
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url =  reverse_lazy("accountapp:hello_world")
+    success_url =  reverse_lazy("home")
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView,MultipleObjectMixin):
@@ -38,7 +38,7 @@ class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
     form_class = AccountUpdateForm
-    success_url =  reverse_lazy("accountapp:hello_world")
+    success_url =  reverse_lazy("home")
     template_name = 'accountapp/update.html'
 
 @method_decorator(has_ownership,'get')
