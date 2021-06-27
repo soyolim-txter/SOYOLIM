a7ece71f66da4b6716433ee2ef81cfa8f3ecc2c4
soyolim-txter <healingdaybut@gmail.com>
2021-06-27 12:11:42 +0900

    add '/'

--- SOYOLIM/settings/deploy.py
+++ SOYOLIM/settings/deploy.py
@@ -1,7 +1,7 @@
 from .base import *
 
 def read_secret(secret_name):
-    file = open('run/secrets/'+secret_name)
+    file = open('/run/secrets/'+secret_name)
     secret = file.read()
     secret = secret.rstrip().lstrip()
     file.close()
